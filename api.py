87ab219e83da731890c98c5f49ffb448c69210b1
ariel-weisman <arielrosew@gmail.com>
2022-06-24 18:47:55 -0400

    removed api key

--- api.py
+++ api.py
@@ -1,12 +1,22 @@
 import mediacloud.api
 import datetime
-mc = mediacloud.api.MediaCloud('apikey')
+import pandas as pd
+import config
+
+#make make sure to put in the api key
+mc = mediacloud.api.MediaCloud(config.mc_API_key)
+
+#set date range for query
 start_date = datetime.date(2020,1,1)
 end_date = datetime.date(2020,5,13)
-date_range_2020 = mc.dates_as_query_clause(start_date, end_date)
-my_query = '(COVID-19 or coronavirus or SARS-CoV-2 or covid) and tags_id_media:34412234'
-results = mc.storyCount(my_query, date_range_2020, split=True, split_period='day')
-results = results.pop('counts', None)
-import pandas as pd
-df = pd.DataFrame.from_dict(results)
-df.to_csv (r'C:\Users\ariel\Downloads\Mediacloud Downloads\export_test.csv', index = False, header=True)
+AAS_date_range = mc.dates_as_query_clause(start_date, end_date)
+
+#import list of kewords to query
+keywords_file = open("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\keywords.txt", "r")
+keyword_list = keywords_file.readlines()
+
+for keyword in keyword_list:
+    counts_by_date = mc.storyCount("(COVID or coronavirus or SARS-CoV-2 or COVID-19)" and keyword, AAS_date_range, split=True, split_period='day') 
+    counts_by_date = counts_by_date.pop('counts', None)
+    df_results = pd.DataFrame.from_dict(counts_by_date)
+    df_results.to_csv ("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\"+f'{keyword}.csv','a', index = False, header=True)
