8ddb25adb0ce1e8c82fd97f54f1d851b11fcfb9a
ariel-weisman <arielrosew@gmail.com>
2022-06-25 17:57:17 -0400

    added totals and improved readme

--- api.py
+++ api.py
@@ -5,17 +5,20 @@ import config
 
 mc = mediacloud.api.MediaCloud(config.my_API_key)
 
-#set date range for query
+#dates selected based on start and end dates of the Ask a Scientist (AAS) Project
 start_date = datetime.date(2020,1,1)
 end_date = datetime.date(2020,5,13)
 AAS_date_range = mc.dates_as_query_clause(start_date, end_date)
 
-#import list of kewords to query
 with open("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\keywords.txt", "r") as keywords_file:
     keyword_list = keywords_file.read().splitlines() 
 
-
+#media tag is for MediaCloud's US-National collection of sources
 for keyword in keyword_list:
-    counts_by_date = mc.storyCount(f"(COVID or coronavirus or SARS-CoV-2 or COVID-19) and {keyword}", AAS_date_range, split=True, split_period='day')
+    counts_by_date = mc.storyCount(f"(COVID or coronavirus or SARS-CoV-2 or COVID-19) and {keyword} and tags_id_media:34412234", AAS_date_range, split=True, split_period='day')
     df_results = pd.DataFrame(counts_by_date["counts"])
     df_results.to_csv (f"C:/Users/ariel/Documents/python_projects/ask_a_scientist/{keyword}.csv", index = False, header=True)
+
+total_counts = mc.storyCount("tags_id_media:34412234",AAS_date_range,split = True,split_period = 'day')
+df_total_results = pd.DataFrame(total_counts["counts"])
+df_total_results.to_csv("C:/Users/ariel/Documents/python_projects/ask_a_scientist/total_stories.csv", index = False, header=True)
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Ask A Scientist MediaCloud
 
-This is for getting media data from the API for the Ask a Scientist project. :)
+This is the code used to query the MediaCloud database for media coverage to be compared to questions data for the Ask A Scientist project.
+
+The Ask a Scientist project was a collaboration between the Federation of American Scientists and the National Science Policy Network during the beginning of the COVID-19 pandemic in the spring of 2020.
+
+Volunteer scientists across the globe took questions from the public regarding COVID-19 and researched the answers, which were posted to the project's website.
+
+To improve science communication efforts, we compared the questions we recieved with concurrent media coverage.
+Keywords were selected based on the content coding system created by the project team to categorize the questions recieved by the team during the project. We hope this research will identify topic areas that had the most uncertainty for the general public.
+
