0d76bcdff37e5e61a1b67ff98f1ed54ffc5fb04a
ariel-weisman <arielrosew@gmail.com>
2022-06-24 20:21:58 -0400

    most recent

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ import datetime
 import pandas as pd
 import config
 
-#make make sure to put in the api key
 mc = mediacloud.api.MediaCloud(config.mc_API_key)
 
 #set date range for query
@@ -12,11 +11,11 @@ end_date = datetime.date(2020,5,13)
 AAS_date_range = mc.dates_as_query_clause(start_date, end_date)
 
 #import list of kewords to query
-keywords_file = open("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\keywords.txt", "r")
-keyword_list = keywords_file.readlines()
+with open("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\keywords.txt", "r") as keywords_file:
+    keyword_list = keywords_file.read().splitlines() 
 
 for keyword in keyword_list:
-    counts_by_date = mc.storyCount("(COVID or coronavirus or SARS-CoV-2 or COVID-19)" and keyword, AAS_date_range, split=True, split_period='day') 
+    counts_by_date = mc.storyCount("(COVID or coronavirus or SARS-CoV-2 or COVID-19)" and keyword, AAS_date_range, split=True, split_period='day')
     counts_by_date = counts_by_date.pop('counts', None)
     df_results = pd.DataFrame.from_dict(counts_by_date)
-    df_results.to_csv ("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\"+f'{keyword}.csv','a', index = False, header=True)
+    df_results.to_csv ("C:\\Users\\ariel\\Documents\\python_projects\\ask_a_scientist\\"+f'{keyword}.csv', index = False, header=True)
